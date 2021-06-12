f96ee2894c8b4f605f43a06125607b026eb93a62
tkmoney11 <tylerkim@outlook.com>
2021-06-11 23:02:41 -0700

    instantiate MessageInput

---
+++ src/components/MessageInput/MessageInput.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+import "./MessageInput.css";
+
+const MessageInput = () => {
+	return <div className="message-input">MessageInput</div>;
+};
+
+export default MessageInput;
--- src/components/pages/MessagePage.js
+++ src/components/pages/MessagePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./MessagePage.css";
+import MessageInput from "./../MessageInput/MessageInput";
 
 const MessagePage = () => {
-	return <div className="message-page">messagepage</div>;
+	return (
+		<div className="message-page">
+			<div>messagepage</div>
+			<MessageInput />
+		</div>
+	);
 };
 
 export default MessagePage;
