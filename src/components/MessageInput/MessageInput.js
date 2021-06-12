bdcefe91f8800c1b6c62b0603b647600a3c5983a
tkmoney11 <tylerkim@outlook.com>
2021-06-11 23:30:56 -0700

    v0 messageInput

--- src/components/MessageInput/MessageInput.js
+++ src/components/MessageInput/MessageInput.js
@@ -1,9 +1,35 @@
-import { React } from "react";
+import { React, useState } from "react";
 
 import "./MessageInput.css";
 
 const MessageInput = () => {
-	return <div className="message-input">MessageInput</div>;
+	const [chatValue, setchatValue] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const msg = chatValue.trim();
+		if (msg.length > 0) {
+			// do something with api here
+			console.log(msg);
+		}
+	};
+
+	const handleChange = (e) => {
+		setchatValue(e.value);
+	};
+
+	return (
+		<form className="chat-bar" onSubmit={(e) => handleSubmit(e)}>
+			<input
+				type="text"
+				className="chat-input"
+				placeholder="say something nice!"
+				value={chatValue}
+				onChange={(e) => handleChange(e)}
+			/>
+			<button>send</button>
+		</form>
+	);
 };
 
 export default MessageInput;
