1b46af0f8c9cda9680b393f868887c6d82e79a06
tkmoney11 <tylerkim@outlook.com>
2021-06-12 00:41:10 -0700

    Message Mechanics

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import MessageInput from "./../MessageInput/MessageInput";
 
 import "./Dialog.css";
-import Messages from "./../Messages/Messages";
+import Message from "../Message/Message";
 
 const Dialog = () => {
+	const [msg, setMsg] = useState([
+		{
+			message:
+				"hello this is a  realllyrealllyrealllyrealllyrealllyrealllyreallly  realllyrealllyrealllyrealllyrealllyrealllyreallly  realllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyrealllyreallly realllyrealllyrealllyrealllyrealllyreallly long message",
+			recipient: "loggedInUser",
+			sender: "otherUser",
+		},
+		{
+			message: `This is v0 of the chatting feature!`,
+			recipient: "otherUser",
+			sender: "loggedInUser",
+		},
+		{
+			message: `We currently support serverless, authenticationless, self messaging only LMFAO`,
+			recipient: "loggedInUser",
+			sender: "otherUser",
+		},
+	]);
+
+	const users = [
+		{ name: "Tyler", username: "loggedInUser" },
+		{ name: "Andrew", username: "otherUser" },
+	];
+
+	const [loggedInUser, setLoggedInUser] = useState(users[0]);
+
+	const handleMessage = (msg, user, i) => {
+		return (
+			<Message
+				userSent={msg.sender == user.username ? true : false}
+				key={i}
+				message={msg.message}
+				name={msg.recipient}
+			/>
+		);
+	};
+
 	return (
 		<div className="dialog">
-			<Messages />
+			{msg.map((msg, i) => {
+				return handleMessage(msg, loggedInUser, i);
+			})}
 			<MessageInput />
 		</div>
 	);
