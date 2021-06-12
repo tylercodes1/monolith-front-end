9f459206128be5f0c7f0f3a16a98bd4144c44262
Annie Tang <49599496+annieztang@users.noreply.github.com>
2021-06-11 23:36:49 -0700

    start users list (#1)

--- src/components/pages/MessagePage.js
+++ src/components/pages/MessagePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./MessagePage.css";
 import Dialog from "./../Dialog/Dialog";
+import UsersList from "./../UsersList/UsersList";
 
 const MessagePage = () => {
 	return (
 		<div className="message-page">
 			<Dialog></Dialog>
+			<UsersList />
 		</div>
 	);
 };
