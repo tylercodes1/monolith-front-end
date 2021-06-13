cf105dcdd9e81997764ddce18f8b7fc558b25f0f
tkmoney11 <tylerkim@outlook.com>
2021-06-13 14:48:04 -0700

    usersList members logic

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -2,13 +2,19 @@ import React, { useContext } from "react";
 import "./UsersList.css";
 import MessagePageContext from "./../pages/Context/MessagePageContext";
 import UserIcon from "./../UserIcon/UserIcon";
+import allGroups from "./../../dummyData";
 
 export default function UserList() {
-	const { currUsersGroups } = useContext(MessagePageContext);
+	const { selectedGroup, allGroups } = useContext(MessagePageContext);
+
+	const members = allGroups.filter(
+		(userGroup) => userGroup.group.groupId === selectedGroup.group.groupId
+	);
+
 	return (
 		<div>
 			<p>Group Members</p>
-			{currUsersGroups.map((userGroup) => (
+			{members.map((userGroup) => (
 				<UserCard
 					key={userGroup.user.userId + userGroup.user.username}
 					user={userGroup.user}
