3b0855e2f5964690a681b3edb798dba58bb869e9
tkmoney11 <tylerkim@outlook.com>
2021-06-12 20:01:00 -0700

    General User icons

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -16,6 +16,7 @@ const Dialog = () => {
 				key={i}
 				message={msg.message}
 				name={msg.user.firstName}
+				user={user}
 			/>
 		);
 	};
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 import "./Message.css";
+import UserIcon from "./../UserIcon/UserIcon";
 
-const Message = ({ prevName, userSent, message, name }) => {
-	console.log(prevName, name);
+const Message = ({ prevName, userSent, message, name, user }) => {
+	console.log(prevName, name, user);
 	return (
 		<div
 			className={
 				userSent ? "message-container user-sent" : "message-container"
 			}
 		>
-			{!userSent && prevName !== name && <div>{name}</div>}
+			{!userSent && prevName !== name && <UserIcon user={user} small />}
 			<p>{message}</p>
-			{userSent && prevName !== name && <div>{name}</div>}
+			{userSent && prevName !== name && <UserIcon user={user} small />}
 		</div>
 	);
 };
