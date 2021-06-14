182842d2de82636a0cb4045337bcb9c5ac2ef574
Annie Tang <49599496+annieztang@users.noreply.github.com>
2021-06-13 19:26:51 -0700

    fix infinite loop (#6)

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -3,41 +3,44 @@ import { Joiner } from "../../helpers";
 import MessagePageContext from "./../pages/Context/MessagePageContext";
 import UserIcon from "./../UserIcon/UserIcon";
 import "./UserMenu.css";
+import { CircularProgress } from "@material-ui/core";
 
 export default function UserMenu() {
-	const { users, setSelectedUser, selectedUser } =
-		useContext(MessagePageContext);
-	return (
-		<div id="user-menu">
-			<p>Select the user you'd like to be!</p>
-			<div id="user-menu-items">
-				{users.map((user) => (
-					<UserMenuItem
-						key={user.userId}
-						user={user}
-						setSelected={setSelectedUser}
-						selected={selectedUser}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  const { users, setSelectedUser, selectedUser } =
+    useContext(MessagePageContext);
+  return (
+    <div id="user-menu">
+      <p>Select the user you'd like to be!</p>
+      {users.length > 0 ? (
+        <div id="user-menu-items">
+          {users.map((user) => (
+            <UserMenuItem
+              key={user.userId}
+              user={user}
+              setSelected={setSelectedUser}
+              selected={selectedUser}
+            />
+          ))}
+        </div>
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
 }
 
 function UserMenuItem({ user, setSelected, selected }) {
-	console.log(selected, user);
-
-	return (
-		<div
-			key={user.userId}
-			className={Joiner(
-				"user-menu-item",
-				selected.userId === user.userId && "selected-user"
-			)}
-			onClick={() => setSelected(user)}
-		>
-			<UserIcon user={user} small />
-			<p>{user.username}</p>
-		</div>
-	);
+  return (
+    <div
+      key={user.userId}
+      className={Joiner(
+        "user-menu-item",
+        selected.userId === user.userId && "selected-user"
+      )}
+      onClick={() => setSelected(user)}
+    >
+      <UserIcon user={user} small />
+      <p>{user.username}</p>
+    </div>
+  );
 }
--- src/components/pages/MessagePage.js
+++ src/components/pages/MessagePage.js
@@ -5,7 +5,6 @@ import UsersList from "./../UsersList/UsersList";
 import GroupsList from "./../GroupsList/GroupsList";
 import MessagePageContext from "./Context/MessagePageContext";
 import allGroups from "./../../dummyData";
-import allMessages from "./../../messagesDummyData";
 import UserMenu from "./../UserMenu/UserMenu";
 import axios from "axios";
 import { CircularProgress } from "@material-ui/core";
@@ -13,103 +12,89 @@ import url from "./../../api/URL";
 import useInterval from "./../../util/useInterval";
 
 const MessagePage = () => {
-	const [users, setUsers] = useState([]);
-	const [selectedUser, setSelectedUser] = useState({
-		email: "string",
-		firstName: "string",
-		lastName: "string",
-		userId: 0,
-		username: "string",
-	});
-	const [groups, setGroups] = useState([
-		{
-			group: {
-				groupId: 0,
-				groupName: "string",
-			},
-			id: 0,
-			user: {
-				email: "string",
-				firstName: "string",
-				lastName: "string",
-				userId: 0,
-				username: "string",
-			},
-		},
-	]);
-	const [selectedGroup, setSelectedGroup] = useState(groups[0]);
-	const [messages, setMessages] = useState([]);
-	const currUsersGroups = getCurrGroups();
-	const [loading, setLoading] = useState(true);
-	const [delay] = useState(1000);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState({});
+  const [groups, setGroups] = useState(allGroups);
+  const [selectedGroup, setSelectedGroup] = useState(groups[0]);
+  const [messages, setMessages] = useState([]);
+  const currUsersGroups = getCurrGroups();
+  const [loading, setLoading] = useState(true);
+  const [delay] = useState(1000);
 
-	// useInterval(async () => {
-	// 	const messagesPromise = axios.get(url + "/message");
-	// 	const res = await Promise.all([messagesPromise]);
-	// 	console.log(res[0].data);
-	// 	setMessages(res[0].data);
-	// }, delay);
+  // useInterval(async () => {
+  // 	const messagesPromise = axios.get(url + "/message");
+  // 	const res = await Promise.all([messagesPromise]);
+  // 	console.log(res[0].data);
+  // 	setMessages(res[0].data);
+  // }, delay);
 
-	const messagePageContext = {
-		currUsersGroups,
-		getMessagesByGroup,
-		groups,
-		messages,
-		users,
-		setSelectedGroup,
-		selectedGroup,
-		setSelectedUser,
-		selectedUser,
-	};
+  useEffect(() => {
+    async function fetchData() {
+      const usersPromise = axios.get(url + "/user");
+      const groupsPromise = axios.get(url + "/groupuser");
+      const messagesPromise = axios.get(url + "/message");
+      const res = await Promise.all([
+        usersPromise,
+        groupsPromise,
+        messagesPromise,
+      ]);
+			const usersRes = res[0].data;
+			const groupsRes = res[1].data;
+      setUsers(usersRes);
+			setSelectedUser(usersRes[0]);
+      setGroups(groupsRes);
+			setSelectedGroup(groupsRes[0]);
+      setMessages(res[2].data);
+    }
+    fetchData();
+    setLoading(false);
+  }, []);
 
-	useEffect(() => {
-		async function fetchData() {
-			const usersPromise = axios.get(url + "/user");
-			const groupsPromise = axios.get(url + "/groupuser");
-			const messagesPromise = axios.get(url + "/message");
-			const res = await Promise.all([
-				usersPromise,
-				groupsPromise,
-				messagesPromise,
-			]);
-			console.log(res);
-			setUsers(res[0].data);
-			// setGroups(res[1].data);
-			setMessages(res[2].data);
-			setLoading(false);
-		}
-		fetchData();
-		setLoading(false);
-	}, []);
+  const messagePageContext = {
+    currUsersGroups,
+    getMessagesByGroup,
+    groups,
+    messages,
+    users,
+    setSelectedGroup,
+    selectedGroup,
+    setSelectedUser,
+    selectedUser,
+  };
 
-	return loading ? (
-		<div className="message-page">
-			<CircularProgress />
-		</div>
-	) : (
-		<MessagePageContext.Provider value={messagePageContext}>
-			<div className="message-page">
-				<UserMenu />
-				<GroupsList />
-				<Dialog />
-				<UsersList />
-			</div>
-		</MessagePageContext.Provider>
-	);
+  return loading ? (
+    <div className="message-page">
+      <CircularProgress />
+    </div>
+  ) : (
+    <MessagePageContext.Provider value={messagePageContext}>
+      <div className="message-page">
+        <UserMenu />
+        <GroupsList />
+        <Dialog />
+        <UsersList />
+      </div>
+    </MessagePageContext.Provider>
+  );
 
-	// helper data-filtering functions
-	function getMessagesByGroup(groupId, messages) {
-		return messages.filter((message) => message.group.groupId === groupId);
-	}
+  // helper data-filtering functions
+  function getMessagesByGroup(groupId, messages) {
+    return messages.filter((message) => message.group.groupId === groupId);
+  }
 
-	function getCurrGroups() {
-		const newCurrGroups = groups.filter(
-			(userGroup) => userGroup.user.userId === selectedUser.userId
-		);
-		if (!newCurrGroups.includes(selectedGroup))
-			setSelectedGroup(newCurrGroups[0]);
-		return newCurrGroups;
-	}
+  function getCurrGroups() {
+    const newCurrGroups = groups.filter(
+      (userGroup) => userGroup.user.userId === selectedUser.userId
+    );
+    if (
+      newCurrGroups.some(
+        (userGroup) => userGroup.group.groupId === selectedGroup.groupId
+      )
+    ) {
+      setSelectedGroup(newCurrGroups[0]);
+    }
+    return newCurrGroups;
+  }
 };
 
 export default MessagePage;
