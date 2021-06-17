53cb3f27f8cc915f62eb593f896683eca2bb0f4c
tkmoney11 <tylerkim@outlook.com>
2021-06-16 23:19:21 -0700

    Good for first loads

--- src/dummyData.js
+++ src/dummyData.js
@@ -2,85 +2,15 @@ const allGroups = [
 	{
 		group: {
 			groupId: 0,
-			groupName: "Burger Lovers",
+			groupName: "Loading data...",
 		},
 		id: 0,
 		user: {
-			email: "abc@gmail.com",
-			firstName: "Annie",
-			lastName: "Tang",
+			email: "hello@world.com",
+			firstName: "Hello",
+			lastName: "World",
 			userId: 0,
-			username: "Tangry",
-		},
-	},
-	{
-		group: {
-			groupId: 0,
-			groupName: "Burger Lovers",
-		},
-		id: 0,
-		user: {
-			email: "def@gmail.com",
-			firstName: "Tyler",
-			lastName: "Kim",
-			userId: 1,
-			username: "GamerGhost99",
-		},
-	},
-	{
-		group: {
-			groupId: 0,
-			groupName: "Burger Lovers",
-		},
-		id: 0,
-		user: {
-			email: "ghi@gmail.com",
-			firstName: "Rosie",
-			lastName: "TheDog",
-			userId: 2,
-			username: "RosieTheDog",
-		},
-	},
-	{
-		group: {
-			groupId: 1,
-			groupName: "Safemoon To the Moon",
-		},
-		id: 1,
-		user: {
-			email: "def@gmail.com",
-			firstName: "Tyler",
-			lastName: "Kim",
-			userId: 1,
-			username: "GamerGhost99",
-		},
-	},
-	{
-		group: {
-			groupId: 2,
-			groupName: "Potato Landers",
-		},
-		id: 1,
-		user: {
-			email: "def@gmail.com",
-			firstName: "Tyler",
-			lastName: "Kim",
-			userId: 1,
-			username: "GamerGhost99",
-		},
-	},
-	{
-		group: {
-			groupId: 2,
-			groupName: "Potato Landers",
-		},
-		id: 1,
-		user: {
-			email: "jkl@gmail.com",
-			firstName: "Kobe",
-			lastName: "TheDog",
-			userId: 3,
-			username: "KobeTheDog",
+			username: "Please wait!",
 		},
 	},
 ];
